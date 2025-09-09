bdf61e8460478869a016b81b353e2b4fdcdce6aa
Arshdeep Singh <213903015+arshdeepsarsh@users.noreply.github.com>
2025-09-09 17:47:57 +0530

    Update embedder.py

--- rag_backend/embedder.py
+++ rag_backend/embedder.py
@@ -1,11 +1,6 @@
-import os
 from openai import OpenAI
-from dotenv import load_dotenv
 
-load_dotenv()
-
-api_key = os.getenv("OPENAI_API_KEY")
-client = OpenAI(api_key=api_key)
+client = OpenAI(api_key="")
 
 def load_embedding_model():
     return client
@@ -16,3 +11,4 @@ def embed_text(text_list, model):
         model="text-embedding-3-small"
     )
     return [embedding.embedding for embedding in response.data]
+
